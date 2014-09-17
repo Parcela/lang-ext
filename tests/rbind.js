651597673831bc21231f523770ba042940d5263c
Marco Asbreuk <marco@itsasbreuk.nl>
2014-09-17 18:46:52 +0200

    fixed tests for IE9

--- tests/rbind.js
+++ tests/rbind.js
@@ -27,10 +27,18 @@ describe('Testing function instance methods', function () {
         it('check nr arguments - same context', function () {
             var rbindfuncEmpty = func.rbind(null),
                 rbindfunc = func.rbind(null, 'append1', 'append2');
+            /*
+            // CAUTIOUS: IE9 thinks "rbindfuncEmpty().length" is an object ??
+            // that's why we changed the next 4 evaluations
             rbindfuncEmpty().length.should.be.eql(0);
             rbindfuncEmpty('first', 'second', 'third').length.should.be.eql(3);
             rbindfunc().length.should.be.eql(2);
             rbindfunc('first', 'second', 'third').length.should.be.eql(5);
+            */
+            (rbindfuncEmpty().length===0).should.be.true;
+            (rbindfuncEmpty('first', 'second', 'third').length===3).should.be.true;
+            (rbindfunc().length===2).should.be.true;
+            (rbindfunc('first', 'second', 'third').length===5).should.be.true;
         });
 
         it('added arguments after arguments - changed context', function () {
@@ -53,10 +61,18 @@ describe('Testing function instance methods', function () {
             var newthis = {},
                 rbindfuncEmpty = func.rbind(newthis),
                 rbindfunc = func.rbind(newthis, 'append1', 'append2');
+            /*
+            // CAUTIOUS: IE9 thinks "rbindfuncEmpty().length" is an object ??
+            // that's why we changed the next 4 evaluations
             rbindfuncEmpty().length.should.be.eql(0);
             rbindfuncEmpty('first', 'second', 'third').length.should.be.eql(3);
             rbindfunc().length.should.be.eql(2);
             rbindfunc('first', 'second', 'third').length.should.be.eql(5);
+            */
+            (rbindfuncEmpty().length===0).should.be.true;
+            (rbindfuncEmpty('first', 'second', 'third').length===3).should.be.true;
+            (rbindfunc().length===2).should.be.true;
+            (rbindfunc('first', 'second', 'third').length===5).should.be.true;
         });
 
 
